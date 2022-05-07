f21066cfd2afa56dc4625c56ed000a0f4c71d021
Olivverde <dele19270@uvg.edu.gt>
2022-05-07 10:11:13 -0600

    Comments and q-table attached

--- CartPole.py
+++ CartPole.py
@@ -3,35 +3,33 @@ import gym
 import time
 import math 
 
-# Carpole has been added
+# Carpole enviroment has been added
 env = gym.make("CartPole-v1")
 print(env.action_space.n)
 
-# Carpole has been added
+# Initial designated variables 
 LEARNING_RATE = 0.1
-
 DISCOUNT = 0.95
 EPISODES = 60000
 total = 0
 total_reward = 0
 prior_reward = 0
-
-Observation = [30, 30, 50, 50]
-np_array_win_size = np.array([0.25, 0.25, 0.01, 0.1])
-
+Observation = [30, 30, 50, 50] # Cart position | Cart Velocity | Pole Angle | Pole Velocity
+np_array_win_size = np.array([0.25, 0.25, 0.01, 0.1]) # Steps for observations vars
+# Epsilon values
 epsilon = 1
-
 epsilon_decay_value = 0.99995
-
+# q-table
 q_table = np.random.uniform(low=0, high=1, size=(Observation + [env.action_space.n]))
 q_table.shape
 
+# Gets the discrete state
 def get_discrete_state(state):
     discrete_state = state/np_array_win_size+ np.array([15,10,1,10])
     return tuple(discrete_state.astype(np.int))
 
 for episode in range(EPISODES + 1): 
-    t0 = time.time() 
+    t0 = time.time() # Added to give time to balance the pole
     discrete_state = get_discrete_state(env.reset()) 
     done = False
     episode_reward = 0 
@@ -42,21 +40,22 @@ for episode in range(EPISODES + 1):
     while not done: 
 
         if np.random.random() > epsilon:
-
+            # Get cordinated action
             action = np.argmax(q_table[discrete_state])
         else:
-
+            # Perform random action
             action = np.random.randint(0, env.action_space.n)
-
+        # States new actions
         new_state, reward, done, _ = env.step(action) 
-
+        # States reward 
         episode_reward += reward
 
         new_discrete_state = get_discrete_state(new_state)
 
         if episode % 2000 == 0:
             env.render()
-
+            print(q_table)
+        # q-table has been updated
         if not done: 
             max_future_q = np.max(q_table[new_discrete_state])
 
@@ -67,7 +66,7 @@ for episode in range(EPISODES + 1):
             q_table[discrete_state + (action,)] = new_q
 
         discrete_state = new_discrete_state
-
+    # Epsilon modification
     if epsilon > 0.05: 
         if episode_reward > prior_reward and episode > 10000:
             epsilon = math.pow(epsilon_decay_value, episode - 10000)
@@ -81,7 +80,7 @@ for episode in range(EPISODES + 1):
 
     total_reward += episode_reward 
     prior_reward = episode_reward
-
+    # Perform 1k episodes after showing avg time & reward
     if episode % 1000 == 0: 
         mean = total / 1000
         print("Time Average: " + str(mean))
